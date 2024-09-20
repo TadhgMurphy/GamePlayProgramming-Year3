a06c1e0402a46d8e9b00ffbbc2451e05beac11ee
Tadhg <c00273712@itcarlow.ie>
2024-09-20 08:20:16 +0100

    Match 3 proper sprites still needed

--- Match-3/Match-3/Match3.cpp
+++ Match-3/Match-3/Match3.cpp
@@ -29,7 +29,7 @@ bool checkForMatches(int grid[GRID_SIZE][GRID_SIZE])
 
 	for (int y = 0; y < GRID_SIZE; y++)
 	{
-		for (int x = 0; x < GRID_SIZE; x++)
+		for (int x = 0; x < GRID_SIZE - 2; x++)
 		{
 			if (grid[y][x] == grid[y][x + 1]
 				&& grid[y][x + 1] == grid[y][x + 2]
@@ -45,7 +45,7 @@ bool checkForMatches(int grid[GRID_SIZE][GRID_SIZE])
 
 	for (int x = 0; x < GRID_SIZE; x++)
 	{
-		for (int y = 0; y < GRID_SIZE; y++)
+		for (int y = 0; y < GRID_SIZE - 2; y++)
 		{
 			if (grid[y][x] == grid[y + 1][x]
 				&& grid[y + 1][x] == grid[y + 2][x]
@@ -73,6 +73,24 @@ void initialiseGrid(int grid[GRID_SIZE][GRID_SIZE])
 	}
 }
 
+void dropTiles(int grid[GRID_SIZE][GRID_SIZE])
+{
+	for (int x = 0; x < GRID_SIZE; x++)
+	{
+		for (int y = GRID_SIZE - 1; y >= 0; y--)
+		{
+			if (grid[y][x] == -1)
+			{
+				for (int k = y; k > 0; k--)
+				{
+					grid[k][x] = grid[k - 1][x]; //Moves tiles down
+				}
+				grid[0][x] = rand() % TILE_TYPES;// Drops new tiles
+			}
+		}
+	}
+}
+
 int main()
 {
 
@@ -89,6 +107,17 @@ int main()
 
 	clockForFrameRate.restart();
 
+	int grid[GRID_SIZE][GRID_SIZE];
+	initialiseGrid(grid);
+
+	sf::Texture tileTextures[TILE_TYPES];
+	for (int i = 0; i < TILE_TYPES; i++)
+	{
+		tileTextures[i].loadFromFile("tile" + std::to_string(i) + ".png");
+	}
+	sf::Vector2i selectedTile(-1, -1);
+	bool tileSelected = false;
+
 	while (window.isOpen())
 	{
 
@@ -97,20 +126,59 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					int x = event.mouseButton.x / TILE_SIZE;
+					int y = event.mouseButton.y / TILE_SIZE;
+					if (!tileSelected)
+					{
+						selectedTile = sf::Vector2i(x, y);
+						tileSelected = true;
+					}
+					else
+					{
+						std::swap(grid[selectedTile.y][selectedTile.x], grid[y][x]);
+						tileSelected = false;
+
+						// Check for matches after swapping
+						if (checkForMatches(grid))
+						{
+							// Drop tiles and keep checking for new matches
+							do
+							{
+								dropTiles(grid);
+							} while (checkForMatches(grid)); // Repeat until no more matches
+						}
+						else
+						{
+							// If no match is found, swap back
+							std::swap(grid[selectedTile.y][selectedTile.x], grid[y][x]);
+						}
+					}
+				}
+			}
 		}
 
-
-		timeSinceLastUpdate += clockForFrameRate.restart();
-
-
-		if (timeSinceLastUpdate > timePerFrame)
+		
+		window.clear();
+		for (int y = 0; y < GRID_SIZE; y++)
 		{
-			window.display();
-
-
-			timeSinceLastUpdate = sf::Time::Zero;
+			for (int x = 0; x < GRID_SIZE; x++)
+			{
+				if (grid[y][x] != -1)
+				{
+					sf::Sprite sprite(tileTextures[grid[y][x]]);
+					sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+					window.draw(sprite);
+				}
+			}
 		}
+
+		window.display();
 	}
 
+	
 	return 0;
 }
