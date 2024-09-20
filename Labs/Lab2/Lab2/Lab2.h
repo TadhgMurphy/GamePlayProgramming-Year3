a3f94532ea1c67e3215d3aa5296e691e47d614b9
C00273712 <c00273712@setu.ie>
2024-09-20 10:42:26 +0100

    Lab1 + Lab2

---
+++ Labs/Lab2/Lab2/Lab2.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+// dont use cout in any of the functions apart from PrintArray();
+int ReplaceWithMinusOne(int array[4][6])
+{
+	return 0;
+}
+void FallDownAndReplace(int array[4][6])
+{
+
+}
+void PrintArray(int array[4][6])
+{
+
+}
